a7588aed5fe266564bf73ecad62d1ce946364dcd
omogbolahan94 <gabrielomogbolahan1@gmail.com>
2025-08-20 20:11:56 +0100

    implemented playwright tools

---
+++ langGraph/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,46 @@
+import nest_asyncio
+from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
+from langchain_community.tools.playwright.utils import create_async_playwright_browser
+import textwrap
+
+# Running a nested eventloop (since python async code only allows for one "event loop" processing aynchronous events)
+nest_asyncio.apply()
+
+
+async_browser =  create_async_playwright_browser(headless=False) 
+toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
+tools = toolkit.get_tools()
+
+# create a dictionary for each tools
+tool_dict = {tool.name:tool for tool in tools}
+print(tool_dict.keys())
+
+# using the tools that navigate the growser and extract text
+navigate_tool = tool_dict.get("navigate_browser")
+extract_text_tool = tool_dict.get("extract_text")
+
+    
+await navigate_tool.arun({"url": "https://www.cnn.com"})
+text = await extract_text_tool.arun({})
+
+
+print(textwrap.fill(text))
+all_tools = tools + [tool_push]
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(all_tools)
+
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+# graph_builder = StateGraph(State)
+# graph_builder.add_node("chatbot", chatbot)
+# graph_builder.add_node("tools", ToolNode(tools=all_tools))
+# graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
+# graph_builder.add_edge("tools", "chatbot")
+# graph_builder.add_edge(START, "chatbot")
+
+# memory = MemorySaver()
+# graph = graph_builder.compile(checkpointer=memory)
+# display(Image(graph.get_graph().draw_mermaid_png()))
--- langGraph/main.py
+++ langGraph/main.py
@@ -1,3 +1,24 @@
+from typing import Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from IPython.display import Image, display
+import gradio as gr
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain.agents import Tool
+
+from pydantic import BaseModel
+import random
+import requests
+import os
+import asyncio
+
+# using playwright
 import nest_asyncio
 from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
 from langchain_community.tools.playwright.utils import create_async_playwright_browser
@@ -6,41 +27,87 @@ import textwrap
 # Running a nested eventloop (since python async code only allows for one "event loop" processing aynchronous events)
 nest_asyncio.apply()
 
-
+# <<<<<<<<<<<<<<<<<<<<<<< TESTING THE PLAYWRIGHT TOOL >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 async_browser =  create_async_playwright_browser(headless=False) 
 toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=async_browser)
 tools = toolkit.get_tools()
 
-for tool in tools:
-    print(f"{tool.name}={tool}")
+# create a dictionary for each tools
+tool_dict = {tool.name:tool for tool in tools}
+print(tool_dict.keys())
 
-# tool_dict = {tool.name:tool for tool in tools}
+# using the tools that navigate the browser and extract text
+async def main_all_tools():
+    # navigate internet tool
+    navigate_tool = tool_dict.get("navigate_browser")    
+    await navigate_tool.arun({
+        "url": "https://www.cnn.com/", 
+        # "timeout": 120000,  # 60s before playwright timeout
+        "wait_until": "commit"
+        })  
 
-# navigate_tool = tool_dict.get("navigate_browser")
-# extract_text_tool = tool_dict.get("extract_text")
+    # extract text tool
+    extract_text_tool = tool_dict.get("extract_text")
+    text = await extract_text_tool.arun({})
+    print(textwrap.fill(text))
 
+
+#  <<<<<<<<<<<<<to run the playwright tools >>>>>>>>>>>>>>>>>>>>>>>>>>
+# asyncio.run(main_all_tools())
+
+# <<<<<<<<<<<<<<<<<<<<<< OTHER TOOLS >>>>>>>>>>>>>>>>>>>>>>>>
+# Built-in serper tool for google web search
+# serper = GoogleSerperAPIWrapper()
+
+# tool_search =Tool(
+#         name="search",
+#         func=serper.run,
+#         description="Useful for when you need more information from an online search"
+#     )
+
+# customised push over app
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_url = "https://api.pushover.net/1/messages.json"
+
+
+def push(text: str):
+    """Send a push notification to the user"""
+    requests.post(pushover_url, data = {"token": pushover_token, "user": pushover_user, "message": text})
     
-# await navigate_tool.arun({"url": "https://www.cnn.com"})
-# text = await extract_text_tool.arun({})
+
+# creeate a tool object for the customized push tool function   
+tool_push = Tool(
+    name="send_push_notification",
+    func=push,
+    description="useful for when you want to send a push notification"
+)
+
+# chain all the playwright tools together with push_tool: we are not using serper tool here
+all_tools = tools + [tool_push]
+
+# <<<<<<<<<<<<<<<<<<<<<< AGENT WORKFLOW >>>>>>>>>>>>>>>>>>>>>>>>
+# define state
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
 
-# print(textwrap.fill(text))
-# all_tools = tools + [tool_push]
-# llm = ChatOpenAI(model="gpt-4o-mini")
-# llm_with_tools = llm.bind_tools(all_tools)
+# chain all tools with LLM 
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(all_tools)
 
 
-# def chatbot(state: State):
-#     return {"messages": [llm_with_tools.invoke(state["messages"])]}
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
-# graph_builder = StateGraph(State)
-# graph_builder.add_node("chatbot", chatbot)
-# graph_builder.add_node("tools", ToolNode(tools=all_tools))
-# graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
-# graph_builder.add_edge("tools", "chatbot")
-# graph_builder.add_edge(START, "chatbot")
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", ToolNode(tools=all_tools))
+graph_builder.add_conditional_edges( "chatbot", tools_condition, "tools")
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
 
-# memory = MemorySaver()
-# graph = graph_builder.compile(checkpointer=memory)
-# display(Image(graph.get_graph().draw_mermaid_png()))
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+display(Image(graph.get_graph().draw_mermaid_png()))
